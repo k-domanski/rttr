089fd81913c06c4bcc2e303dc91807cdd4a129e0
k-dom <224285@edu.p.lodz.pl>
2021-03-23 14:20:54 +0100

    added version.h and premake; updated gitignore

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "rttr"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/rttr/**.h",
+        "src/rttr/**.cpp",
+        "src/rttr/**.c"
+    }
+
+    includedirs
+    {
+        "src"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
